d7613dcfaf7c1174f85e5f116ab65713b7629ebc
tronilo53 <tronilo53@gmail.com>
2023-06-23 18:35:55 +0200

    Desinstalado apexcharts ng-apexcharts

--- src/app/pages/home/main/detail-device/detail-device.component.ts
+++ src/app/pages/home/main/detail-device/detail-device.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { ApexNonAxisChartSeries, ApexChart, ApexResponsive } from "ng-apexcharts";
+import { DataService } from 'src/app/services/data.service';
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-detail-device',
@@ -9,10 +18,20 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class DetailDeviceComponent implements OnInit {
 
   public id: string = '';
+  public chartOptions: ChartOptions;
+  private dataDevice: any[] = [];
 
-  constructor( private activatedRoute: ActivatedRoute ) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private __dataService: DataService 
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((p: Params) => this.id = p['id']);
+    this.activatedRoute.params.subscribe((p: Params) => {
+      this.id = p['id'];
+      this.__dataService.getDevices().subscribe((e: any) => {
+        this.dataDevice = e;
+      });
+    });
   }
 }
