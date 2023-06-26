96f325ad7a831c51fbc173922d9ba9dac2fc73b4
tronilo53 <tronilo53@gmail.com>
2023-06-26 14:04:10 +0200

    Pipe de info de dispositivo Completado. Implementada interfaz de DetailDevice

--- src/app/pipes/model.pipe.ts
+++ src/app/pipes/model.pipe.ts
@@ -5,13 +5,29 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ModelPipe implements PipeTransform {
 
-  transform(value: string): string {
-    let model = 'Test';
+  transform(value: string, args?: string): string {
+    let model = '';
+    let potMax = '';
+    let potMIn = '';
+    let frecMax = '';
+    let frecMin = '';
     if(value) {
       if( value.indexOf('GADTEST') > -1 ) model = 'Alta';
       else model = 'Media';
-    }
-    return model;
+      if( !args ) return model;
+      else {
+        if( args === 'potMax' ) {
+          if( model === 'Alta' ) return '8';
+          else return '5'
+        }
+        else if( args === 'potMin' ) return '0';
+        else if( args === 'frecMax' ) {
+          if( model === 'Alta' ) return '13,5 kHz';
+          else return '10,5 kHz';
+        }
+        else return '6,5 kHz';
+      }
+    }else return 'Sin info...';
   }
 
 }
