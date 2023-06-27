75b07a2b08961b671c68c6388b5078cd37b72cb9
tronilo53 <tronilo53@gmail.com>
2023-06-27 20:30:36 +0200

    Implementada logica para cambiar avatar. Pruebas hechas con Cookies, llevar a cabo la misma logica con el usuario actual

--- src/app/pages/home/profile/avatar/avatar.component.ts
+++ src/app/pages/home/profile/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -6,7 +6,7 @@ import Swal from 'sweetalert2';
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.css']
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnInit, AfterViewInit {
 
   public usersTest: any[] = [
     { username: 'tronilo53', name: 'Luis', email: 'tronilo53@gmail.com', creation: '27/06/23', status: 'enabled', rol: 'admin', avatar: 'Daredevil-256.png' },
@@ -24,46 +24,54 @@ export class AvatarComponent implements OnInit {
     'Silver-Surfer-256.png', 
     'Superman-256.png', 
     'the-Thing-256.png' ];
+  @ViewChildren('avatarsImage') avatarImages: QueryList<ElementRef>;
 
-  constructor() {}
+  constructor( private renderer: Renderer2 ) {}
 
   ngOnInit(): void {
     
   }
+  ngAfterViewInit(): void {
+    for( let i = 0; i < this.avatarImages.toArray().length; i++ ) {
+      if( this.avatarImages.toArray()[i].nativeElement.childNodes[0].alt === localStorage.getItem( 'avatar' ) ) {
+        this.renderer.addClass( this.avatarImages.toArray()[i].nativeElement, 'disabled' );
+      }
+    }
+  }
 
   public changeAvatar( avatar: string ) {
-    Swal.fire({
-      html:`
-        <div class="container">
-          <div class="row align-items-center text-center">
-            <div class="col-5">
-              <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
-                <img src="../../../../../assets/avatars/${avatar}" style="width: 7rem;"/>
+    if( avatar !== localStorage.getItem( 'avatar' ) ) {
+      Swal.fire({
+        html:`
+          <div class="container">
+            <div class="row align-items-center text-center">
+              <div class="col-5">
+                <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
+                  <img src="../../../../../assets/avatars/${localStorage.getItem('avatar')}" style="width: 7rem;"/>
+                </div>
               </div>
-            </div>
-            <div class="col-2">
-              <span class="material-symbols-outlined" style="font-size: 3rem; vertical-align: middle;">arrow_forward_ios</span>
-            </div>
-            <div class="col-5">
-              <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
-                <img src="../../../../../assets/avatars/${avatar}" style="width: 7rem;"/>
+              <div class="col-2">
+                <span class="material-symbols-outlined" style="font-size: 3rem; vertical-align: middle;">arrow_forward_ios</span>
+              </div>
+              <div class="col-5">
+                <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
+                  <img src="../../../../../assets/avatars/${avatar}" style="width: 7rem;"/>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      `,
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
-      }
-    })
+        `,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Ahora no',
+        confirmButtonText: 'Adelante!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.setItem( 'avatar', avatar );
+          history.back();
+        }
+      })
+    }
   }
 }
--- src/app/pages/home/profile/info/info.component.ts
+++ src/app/pages/home/profile/info/info.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements AfterViewInit {
 
+  public avatar: string = '';
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    if( localStorage.getItem('avatar') ) this.avatar = localStorage.getItem('avatar');
+    else this.avatar = 'default.png';
+  }
 }
