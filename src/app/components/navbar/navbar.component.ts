8bf4a17359d3d6669d25a168764a0221d320c023
tronilo53 <tronilo53@gmail.com>
2023-06-28 10:50:33 +0200

    Movido test de usuarios al componente NavbarComponent: La logica de usuarios logueados tendria que estar aqui para el acceso general en componentes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  public usersTest: any[] = [
+    { username: 'tronilo53', name: 'Luis', email: 'tronilo53@gmail.com', creation: '27/06/23', status: 'enabled', rol: 'admin', avatar: 'Daredevil-256.png' },
+    { username: 'tironlan', name: 'Tirion', email: 'tirionlan@gmail.com', creation: '26/06/23', status: 'enabled', rol: 'user', avatar: 'Namor-256.png' },
+    { username: 'kalesi', name: 'Daneris', email: 'kalesidragons@gmail.com', creation: '20/06/23', status: 'enabled', rol: 'admin', avatar: 'Invisible-Woman-256.png' }
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 }
