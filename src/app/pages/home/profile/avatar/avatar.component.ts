cf0462bf61a81d9f755ee9e520ef94da4b004499
tronilo53 <tronilo53@gmail.com>
2023-06-30 17:23:21 +0200

    Implementando obtencion de usuarios para pruebas. Creado método para obtener usuario a partir de un id, creada interfaz para tipado. mostrando resultado en ngOninit de AvatarComponent y eliminado modal personalizado en el html

--- src/app/pages/home/profile/avatar/avatar.component.ts
+++ src/app/pages/home/profile/avatar/avatar.component.ts
@@ -1,11 +1,12 @@
-import { AfterViewInit, Component, ElementRef, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChildren } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-avatar',
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.css']
 })
-export class AvatarComponent implements AfterViewInit {
+export class AvatarComponent implements OnInit, AfterViewInit {
 
   public avatars: string[] = [ 
     'Batman-256.png', 
@@ -22,8 +23,17 @@ export class AvatarComponent implements AfterViewInit {
   ];
   @ViewChildren('avatarsImage') avatarImages: QueryList<ElementRef>;
 
-  constructor( private renderer: Renderer2 ) {}
-
+  constructor( 
+    private renderer: Renderer2,
+    private __dataService: DataService
+  ) {}
+  
+  ngOnInit(): void {
+    const data: any = { id: '1' };
+    this.__dataService.getUser(data).subscribe((e: any) => {
+      console.log(e.res);
+    });
+  }
   ngAfterViewInit(): void {
    
     /*for( let i = 0; i < this.avatarImages.toArray().length; i++ ) {
@@ -34,42 +44,40 @@ export class AvatarComponent implements AfterViewInit {
   }
 
   public changeAvatar( avatar: string ) {
-    /*this.__comunicationService.getAvatar().subscribe(e => {
-      if( avatar !== e ) {
-        Swal.fire({
-          html:`
-            <div class="container">
-              <div class="row align-items-center text-center">
-                <div class="col-5">
-                  <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
-                    <img src="../../../../../assets/avatars/${localStorage.getItem('avatar')}" style="width: 7rem;"/>
-                  </div>
+    /*if( avatar !== e ) {
+      Swal.fire({
+        html:`
+          <div class="container">
+            <div class="row align-items-center text-center">
+              <div class="col-5">
+                <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
+                  <img src="../../../../../assets/avatars/${localStorage.getItem('avatar')}" style="width: 7rem;"/>
                 </div>
-                <div class="col-2">
-                  <span class="material-symbols-outlined" style="font-size: 3rem; vertical-align: middle;">arrow_forward_ios</span>
-                </div>
-                <div class="col-5">
-                  <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
-                    <img src="../../../../../assets/avatars/${avatar}" style="width: 7rem;"/>
-                  </div>
+              </div>
+              <div class="col-2">
+                <span class="material-symbols-outlined" style="font-size: 3rem; vertical-align: middle;">arrow_forward_ios</span>
+              </div>
+              <div class="col-5">
+                <div style="padding: 5px; background-color: #494949; display: inline-block; border-radius: 10px;">
+                  <img src="../../../../../assets/avatars/${avatar}" style="width: 7rem;"/>
                 </div>
               </div>
             </div>
-          `,
-          showCancelButton: true,
-          confirmButtonColor: '#3085d6',
-          cancelButtonColor: '#d33',
-          cancelButtonText: 'Ahora no',
-          confirmButtonText: 'Adelante!'
-        }).then((result) => {
-          if (result.isConfirmed) {
-            localStorage.setItem( 'avatar', avatar );
-            this.__comunicationService.setAvatar(avatar);
-            this.__comunicationService.getAvatar().subscribe(e => console.log(e));
-            history.back();
-          }
-        })
-      }
-    });*/
+          </div>
+        `,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Ahora no',
+        confirmButtonText: 'Adelante!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.setItem( 'avatar', avatar );
+          this.__comunicationService.setAvatar(avatar);
+          this.__comunicationService.getAvatar().subscribe(e => console.log(e));
+          history.back();
+        }
+      })
+    }*/
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface User {
+  id: number;
+  username: string;
+  name: string;
+  email: string;
+  creation: Date;
+  status: string;
+  role: string;
+  avatar: string;
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -10,5 +20,8 @@ export class DataService {
 
   public getDevices() {
     return this.__httpClient.get( '../../assets/json/devices.json' );
+  }
+  public getUser( data: any ) {
+    return this.__httpClient.post( 'http://localhost/backend/getUser.php', JSON.stringify(data) );
   }
 }
