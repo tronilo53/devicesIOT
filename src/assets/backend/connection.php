c42b5aff02b1c2f889c47a13c14612d1ed8e1b53
tronilo53 <tronilo53@gmail.com>
2023-07-01 22:02:33 +0200

    Implementando comprobador de contraseñas en tiempo real en RegisterComponent

--- src/assets/backend/connection.php
+++ src/assets/backend/connection.php
@@ -5,7 +5,4 @@
     $con = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
     $pdo = new PDO( $con, $db_user, $db_pass );
 
-    if($pdo) echo 'Conexión satisfactoria con BBDD freelsdevcamp.es';
-    else echo 'No se ha podido conectar a la BBDD freelsdevcamp.es';
-
 ?>
