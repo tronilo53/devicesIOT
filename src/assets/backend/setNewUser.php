f9f54514be43f0bd38b490504eeeb0e2d7a4eafb
tronilo53 <tronilo53@gmail.com>
2023-07-04 12:51:56 +0200

    Implementado registrar nuevo usuario en registerComponent, servidor responde con objeto del ultimo usuario registrado. Pendiente envio de correo electronico para verificar identidad

--- src/assets/backend/setNewUser.php
+++ src/assets/backend/setNewUser.php
@@ -10,7 +10,17 @@
     $stmt = $pdo -> prepare( 'SELECT COUNT(*) FROM users WHERE email = ?' );
     $stmt -> execute([ $params->email ]);
     if( $stmt -> fetchColumn() > 0 ) $res = [ "res" => "User exist" ];
-    else $res = [ 'res' => 'User not exist' ];
+    else {
+        $stmt = $pdo -> prepare('INSERT INTO users(name,email) VALUES(?,?)');
+        $stmt -> execute([ $params->name, $params->email ]);
+        if( !$stmt ) $res = [ 'res' => 'Error to Insert the User' ];
+        else {
+            $last_id = $pdo -> lastInsertId();
+            $stmt = $pdo -> prepare('SELECT id,name,email,creation,status,role,avatar FROM users WHERE id = ?');
+            $stmt -> execute([ $last_id ]);
+            $res = $stmt -> fetchAll();
+        }
+    }
     
     header('Content-Type: application/json');
 
