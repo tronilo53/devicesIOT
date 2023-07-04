be094a5454080d4358198eee6124dfc5a67f1c05
tronilo53 <tronilo53@gmail.com>
2023-07-04 21:31:53 +0200

    configurando PHPMailer en setNewUser.php. Creada carpeta 'tests' en Assets con archivo html para reemplazarlo en el cuerpo del email. Nota: utilizar solo HTML 4.01 Por temas de compatibilidad y posibles errores

--- src/assets/backend/setNewUser.php
+++ src/assets/backend/setNewUser.php
@@ -2,6 +2,10 @@
     
     require_once 'cors.php';
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
     $json = file_get_contents('php://input');
     $params = json_decode($json);
 
@@ -17,10 +21,37 @@
         $stmt -> execute([ $params->name, $params->email, $password_hash, $token ]);
         if( !$stmt ) $res = [ 'res' => 'Error to Insert the User' ];
         else {
-            $last_id = $pdo -> lastInsertId();
+            /*$last_id = $pdo -> lastInsertId();
             $stmt = $pdo -> prepare('SELECT * FROM users WHERE id = ?');
             $stmt -> execute([ $last_id ]);
-            $res = $stmt -> fetchAll();
+            $res = $stmt -> fetchAll();*/
+            try {
+                $mail->isSMTP();
+                $mail->SMTPAuth = true;
+                // Datos personales
+                $mail->Host = "smtp.ionos.es";
+                $mail->Port = "587";
+                $mail->Username = "info@freelsdevcamp.es";
+                $mail->Password = "@Shutdown@";
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                // Remitente
+                $mail->setFrom('info@freelsdevcamp.es', 'freelsdevcamp');
+                // Destinatario, opcionalmente también se puede especificar el nombre
+                $mail->addAddress($params->email, $params->name);
+                $mail->isHTML(true);
+                // Asunto
+                $mail->Subject = 'Verificación de Cuenta';
+                // Contenido HTML
+                $mail->Body = '
+                    
+                ';
+                $mail->AltBody = 'El texto como elemento de texto simple';
+                // Agregar archivo adjunto
+                $mail->addAttachment("/home/user/Desktop/ejemplodeimagen.png", "ejemplodeimagen.png");
+            } catch (Exception $e) {
+                $res = [ 'res' => 'error: ' . $e ];
+            }
         }
     }
     
